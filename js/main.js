4bd32fc7c979721fa96e4bb8a0e6c33d331042ef
Greg Rowe <growe22@ufl.edu>
2016-11-04 17:21:55 -0500

    add JavaScript to create element for click counter

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ var clickCounter = 0;
 
 function recordClicks() {
     clickCounter = clickCounter + 1;
-    console.log(clickCounter);
+    var paragraph = document.createElement('p');
+    var node = document.createTextNode('This is click number ' + clickCounter);
+    paragraph.appendChild(node);
+    var element = document.getElementById('clicks-h1');
+    element.appendChild(paragraph);
 }
 
 heading.addEventListener('click', recordClicks);
