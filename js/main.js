25792c0a64f3a99af9589ba06dfc78a5c5bfbe7b
Greg Rowe <growe22@ufl.edu>
2016-11-04 20:44:51 -0500

    add JavaScript to listen for keydown and add h1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//This code listens for a click on the "main-heading" and changes the text to "DA Boys"
 var heading = document.getElementById('main-heading');
 
 function modifyHTML() {
@@ -7,7 +8,7 @@ function modifyHTML() {
 
 heading.addEventListener('click', modifyHTML);
 
-
+//This code listens for a mouseover on the "sub-heading" and changes the text color to white
 var subHeading = document.getElementById('sub-heading');
 
 function changeColor() {
@@ -16,3 +17,14 @@ function changeColor() {
 }
 
 subHeading.addEventListener('mouseover', changeColor);
+
+//This code listens for a keydown (press any key) and adds some h1 content "They could go all the way!"
+function addContent() {
+    var newHeading = document.createElement('h1');
+    var node = document.createTextNode('They Could Go All The Way!');
+    newHeading.appendChild(node);
+    var newElement = document.getElementById('container');
+    newElement.appendChild(newHeading);
+}
+
+document.addEventListener('keydown', addContent);
