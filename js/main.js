7711676dfa5dd957f435cf8733fd09d9459e5cc3
Greg Rowe <growe22@ufl.edu>
2016-11-04 20:06:12 -0500

    recode JavaScript for form logging

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
+var checkForSubmit = document.getElementById('the-form');
 var firstName = document.getElementById('first-name');
-
 var lastName = document.getElementById('last-name');
-
 var email = document.getElementById('email');
-
 var message = document.getElementById('message');
 
-var submitClick = document.getElementsByName('form');
-submitClick.addEventListener('submit', function (event) {
-    event.preventDefault();
+function formLog() {
+    var submitClick = document.getElementById('the-form');
+    submitClick.preventDefault('submit');
     console.log('The firstName is: ' + firstName);
     console.log('The lastName is: ' + lastName);
     console.log('The email is: ' + email);
     console.log('The message is: ' + message);
-});
+}
+
+checkForSubmit.addEventListener('submit', formLog);
